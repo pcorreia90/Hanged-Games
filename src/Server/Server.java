2740ac79d7882973ae0514d2d8d814834cc83fab
Pedro <pcorreia90@gmail.com>
2022-03-04 11:34:10 +0000

    Created tell everyone method

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,15 +1,17 @@
 package Server;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.lang.management.PlatformLoggingMXBean;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Server {
     private static final int PORTNUM = 8081;
     private ServerSocket serverSocket;
     private Game game;
+    private List<PlayerHandler> playerList = new ArrayList<>();
 
     public Server() {
         try {
@@ -33,6 +35,18 @@ public class Server {
         }
     }
 
+    public void tellEveryone(String message) {
+        for (PlayerHandler player : playerList) {
+            try {
+                PrintWriter writer = new PrintWriter(player.clientSocket.getOutputStream(), true);
+                writer.println(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+
+            }
+        }
+    }
+
     public class PlayerHandler implements Runnable{
         private Socket clientSocket;
         private String message;
@@ -48,6 +62,7 @@ public class Server {
                 while(!clientSocket.isClosed()){
                     message = msgReader.readLine();
                     game.printMessage(message);
+                    tellEveryone(message);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
